a5e420ebc34d39a9613553ae834d6ddf59ad0132
LamYiYong <120798992+LamYiYong@users.noreply.github.com>
2024-07-11 20:02:39 +0800

    Add files via upload

---
+++ Satellite.h
@@ -0,0 +1,11 @@
+//===================================================================
+// File: spaceship.h
+// Task: Functions that draw Satellite.
+//===================================================================
+
+#ifndef SATELLITE_H
+#define SATELLITE_H
+
+void Satellite();
+
+#endif
---
+++ SpaceStation.h
@@ -0,0 +1,11 @@
+//===================================================================
+// File: spaceship.h
+// Task: Functions that draw SpaceStation.
+//===================================================================
+
+#ifndef SPACESTATION_H
+#define SPACESTATION_H
+
+void SpaceStation();
+
+#endif
